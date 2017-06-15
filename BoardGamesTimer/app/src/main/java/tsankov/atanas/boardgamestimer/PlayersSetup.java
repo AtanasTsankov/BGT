fa3160ee0613764f40826e9a0e0f6cbc6ef6f48f
Atanas Tsankov <atanas1914@gmail.com>
2017-06-15 17:03:57 +0300

    Added dynamic creation of EditText field for every player to input their names.

--- BoardGamesTimer/app/src/main/java/tsankov/atanas/boardgamestimer/PlayersSetup.java
+++ BoardGamesTimer/app/src/main/java/tsankov/atanas/boardgamestimer/PlayersSetup.java
@@ -3,6 +3,9 @@ package tsankov.atanas.boardgamestimer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
@@ -14,5 +17,31 @@ public class PlayersSetup extends AppCompatActivity {
         setContentView(R.layout.activity_players_setup);
         Intent intent = getIntent();
         HashMap value = (HashMap) intent.getSerializableExtra("DataMap");
+        Integer players = (Integer) value.get("P_Count");
+
+        LinearLayout m_vwJokeLayout = (LinearLayout) this.findViewById(R.id.lnrLayoutPlayers);
+
+        for (int i = 1; i <= players; i++){
+
+
+        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        EditText tv= new EditText(this);
+        tv.setLayoutParams(lparams);
+        tv.setHint("Player " + i + " name");
+        m_vwJokeLayout.addView(tv);
+
+        }
+
+//        final EditText[] playersNames = new EditText[players];
+//
+//        LinearLayout myLayout = (LinearLayout)findViewById(R.id.linearLayoutPlayers);
+//
+//        for(int i = 1; i <= players; i++){
+//            final EditText plName = new EditText(this);
+//            plName.setHint("Player " + i + " name");
+//            myLayout.addView(plName);
+//            playersNames[i] = plName;
+//        }
     }
 }
