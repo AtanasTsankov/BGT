deed4ddbd912c0de129be3fd3908a3b4917cabbb
Atanas Tsankov <atanas1914@gmail.com>
2017-06-16 15:58:12 +0300

    Trying to fix the dynamic addition of editTextFields under each other.

--- BoardGamesTimer/app/src/main/java/tsankov/atanas/boardgamestimer/PlayersSetup.java
+++ BoardGamesTimer/app/src/main/java/tsankov/atanas/boardgamestimer/PlayersSetup.java
@@ -3,8 +3,10 @@ package tsankov.atanas.boardgamestimer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -19,19 +21,28 @@ public class PlayersSetup extends AppCompatActivity {
         HashMap value = (HashMap) intent.getSerializableExtra("DataMap");
         Integer players = (Integer) value.get("P_Count");
 
-        LinearLayout m_vwJokeLayout = (LinearLayout) this.findViewById(R.id.lnrLayoutPlayers);
+        RelativeLayout m_vwJokeLayout = (RelativeLayout) this.findViewById(R.id.relLayoutPlayers);
+        RelativeLayout.LayoutParams lparams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-        for (int i = 1; i <= players; i++){
 
 
-        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        EditText tv= new EditText(this);
-        tv.setLayoutParams(lparams);
-        tv.setHint("Player " + i + " name");
-        m_vwJokeLayout.addView(tv);
+        for (int i = 2; i <= players; i++){
+            int idLastChild= m_vwJokeLayout.getChildAt(m_vwJokeLayout.getChildCount()-1).getId();
+            lparams.addRule(RelativeLayout.BELOW, idLastChild);
+            EditText tv= new EditText(this);
+            tv.setId(idLastChild+1);
+            tv.setLayoutParams(lparams);
+            tv.setHint("Player " + i + " name");
+            m_vwJokeLayout.addView(tv);
+
 
         }
+
+//        Button btn = new Button(this);
+//        btn.setLayoutParams(lparams);
+//        btn.setText("CONTNINUE");
+//        btn.setPo
 
 //        final EditText[] playersNames = new EditText[players];
 //
